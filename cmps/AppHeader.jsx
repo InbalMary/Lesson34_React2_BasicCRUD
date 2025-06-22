fdcf9ddf9e44dad0e553b72fa6eca3bd30d7fb97
InbalMary <inbalby10@gmail.com>
2025-06-22 15:08:23 +0300

    Add effect from  animate.css

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,15 +1,25 @@
-const { Link, NavLink } = ReactRouterDOM
+import { animateCSS } from "../services/util.service.js"
 
-export function AppHeader({ onSetPage, currentPage }) {
+const { Link, NavLink } = ReactRouterDOM
+const { useRef } = React
+
+export function AppHeader() {
+
+    const elH1 = useRef(null)
+
+    function handleLinkClick(ev) {
+        console.log('ev inside handel', ev)
+        animateCSS(elH1.current, 'pulse')
+    }
 
     return (
         <header className="app-header container">
             <section>
-                <h1>Miss Books</h1>
+                <h1 ref={elH1}>Miss Books</h1>
                 <nav className="app-nav">
-                    <NavLink to="/home" >Home</NavLink>
-                    <NavLink to="/about" >About</NavLink>
-                    <NavLink to="/book" >Books</NavLink>
+                    <NavLink onClick={handleLinkClick} to="/home" >Home</NavLink>
+                    <NavLink onClick={handleLinkClick} to="/about" >About</NavLink>
+                    <NavLink onClick={handleLinkClick} to="/book" >Books</NavLink>
                 </nav>
             </section>
         </header>
